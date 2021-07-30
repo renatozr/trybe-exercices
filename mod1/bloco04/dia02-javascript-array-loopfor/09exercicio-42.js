27b1f53913a2251036971628bf4c0f2eade05864
renatozr <renatozr07@gmail.com>
2021-07-29 21:31:32 -0300

    exercicios 4.2

---
+++ mod1/bloco04/dia02-javascript-array-loopfor/09exercicio-42.js
@@ -0,0 +1,10 @@
+let array = [];
+
+for (let i = 1; i <= 25; i += 1) {
+    array.push(i);
+}
+
+for (let i2 = 0; i2 < array.length; i2 += 1) {
+    let result = array[i2]/2;
+    console.log(result);
+}
