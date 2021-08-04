e7cbd55fd6a1c5209d6d3fa313ee4cc71ff85e7d
renatozr <renatozr07@gmail.com>
2021-08-04 15:49:51 -0300

    exercicio 04

--- mod1/bloco04/dia04-objetos-funcoes/03exercicio-44.js
+++ mod1/bloco04/dia04-objetos-funcoes/03exercicio-44.js
@@ -1,5 +1,14 @@
-let arrayInteiros = [2, 3, 6, 7, 10, 1];
-
-function indexHighValue (array) {
-
+function indexHighValue(array) {
+  let arrayHighValue = array;
+  let HighValue = arrayHighValue[0];
+  let indexResult;
+  for (let i = 0; i < arrayHighValue.length; i += 1) {
+    if (arrayHighValue[i] > HighValue) {
+      HighValue = arrayHighValue[i];
+      indexResult = i;
+    }
+  }
+  return indexResult;
 }
+
+console.log(indexHighValue([2, 3, 6, 7, 10, 1]));
