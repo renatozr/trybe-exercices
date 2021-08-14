340c2d44729c5c6a57e4f43a828dc502b8f9a9ab
renatozr <renatozr07@gmail.com>
2021-08-13 22:58:14 -0300

    feito ex 1 e 2

--- mod1/bloco05/dia03-eventos/script.js
+++ mod1/bloco05/dia03-eventos/script.js
@@ -14,3 +14,40 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const ulDays = document.getElementById('days');
+
+function createDaysCalendar() {
+  for (let day of dezDaysList) {
+
+    const li = document.createElement('li');
+    li.innerText = day;
+    li.className = 'day';
+
+    if (day === 24 || day === 25 || day === 31) {
+      li.className += ' holiday';
+    }
+
+    if (day === 4 || day === 11 || day === 18 || day == 25) {
+      li.className += ' friday';
+    }
+
+    ulDays.appendChild(li);
+  }
+}
+
+createDaysCalendar();
+
+
+const divButtonsContainer = document.querySelector('.buttons-container');
+
+function createButtonFeriados(string) {
+  const buttonFeriados = document.createElement('button');
+  buttonFeriados.innerText = string;
+  buttonFeriados.id = 'btn-holiday';
+
+  divButtonsContainer.appendChild(buttonFeriados);
+}
+
+createButtonFeriados('Feriados');
+
