084803abb92b1ffa594012a9cc1976026a8981cd
renatozr <renatozr07@gmail.com>
2021-08-18 18:23:52 -0300

    exercicios 6.1

---
+++ mod1/bloco06/dia01-html-css-forms/script.js
@@ -0,0 +1,7 @@
+let arraySiglaEstado = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RO', 'RS', 'RR', 'SC', 'SE', 'SP', 'TO'];
+
+let arrayTextoEstado = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+
+const selectEstado = document.getElementById('select-estado');
+
+console.log(arraySiglaEstado.length, arrayTextoEstado.length);
