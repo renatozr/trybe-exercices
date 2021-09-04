41e95ea8e4254b96323d7aec391b88925e68538a
renatozr <renatozr07@gmail.com>
2021-09-03 21:27:08 -0300

    faz exercise3 8.2

--- fundamentos/bloco-08-higher-order-functions-jsES6/dia-2-es6-hof-foreach-find-some-every-sort/exercise3.js
+++ fundamentos/bloco-08-higher-order-functions-jsES6/dia-2-es6-hof-foreach-find-some-every-sort/exercise3.js
@@ -64,3 +64,21 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  // escreva seu código aqui
+  const result = books.find(element => element.name.length === 26);
+  return result;
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
