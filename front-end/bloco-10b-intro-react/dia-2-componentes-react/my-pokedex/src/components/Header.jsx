065ebe15c0dbf4236f5f9923289d827bb3565b1a
renatozr <renatozr07@gmail.com>
2021-10-08 20:59:14 -0300

    exercicios 10.2

---
+++ front-end/bloco-10b-intro-react/dia-2-componentes-react/my-pokedex/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>My</h1><img src="https://cdn2.bulbagarden.net/upload/4/4b/Pok%C3%A9dex_logo.png" alt="Logo Pokedex" />
+      </header>
+    );
+  }
+}
+
+export default Header;
