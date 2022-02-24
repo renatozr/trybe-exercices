ad7fa049ed48208b9b7e42dae7e08aab0a0aade5
renatozr <renatozr07@gmail.com>
2022-02-23 22:56:17 -0300

    ex completos, dia 22.4

--- 3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/http-exercise/index.js
+++ 3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/http-exercise/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const PORT = 3000;
 const app = express();
@@ -28,6 +29,64 @@ app.put('/users/:name/:age', (req, res) => {
   const { name, age } = req.params;
 
   res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
+
+app.get('/simpsons', async (_req, res, next) => {
+  try {
+    const simpsonsJson = await fs.readFile('./simpsons.json');
+    const simpsons = JSON.parse(simpsonsJson);
+  
+    res.status(200).json(simpsons);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/simpsons/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const simpsonsJson = await fs.readFile('./simpsons.json');
+    const simpsons = JSON.parse(simpsonsJson);
+
+    const selectedSimpson = simpsons.find((s) => s.id === id);
+
+    if (!selectedSimpson) res.status(404).json({ message: 'simpson not found' });
+
+    res.status(200).json(selectedSimpson);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post('/simpsons', async (req, res, next) => {
+  try {
+    const { id, name } = req.body;
+
+    const simpsonsJson = await fs.readFile('./simpsons.json');
+    const simpsons = JSON.parse(simpsonsJson);
+  
+    const isIdAlreadyExists = simpsons.some((s) => s.id === id);
+
+    if (isIdAlreadyExists) res.status(409).json({ message: 'id already exists' });
+
+    const newSimpsons = [...simpsons, { id, name }];
+    const newSimpsonsJson = JSON.stringify(newSimpsons);
+
+    await fs.writeFile('./simpsons.json', newSimpsonsJson);
+
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.all('*', (req, res) => {
+  res.status(404).json({ message: `A rota '${req.path}' não existe`});
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(500).json({ message: `${err}` });
 });
 
 app.listen(PORT, () => {
