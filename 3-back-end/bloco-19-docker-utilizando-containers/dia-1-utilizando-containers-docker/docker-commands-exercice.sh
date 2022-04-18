fe3b1476c01d5d7af26ae89d42a9ee66385ed301
Renato Zanella <renatozr07@gmail.com>
2022-04-18 17:10:10 -0300

    exercicio dia 19.1

--- 3-back-end/bloco-19-docker-utilizando-containers/dia-1-utilizando-containers-docker/docker-commands-exercice.sh
+++ 3-back-end/bloco-19-docker-utilizando-containers/dia-1-utilizando-containers-docker/docker-commands-exercice.sh
@@ -1,2 +1,13 @@
 docker image pull debian:stable-slim
 
+docker container run -it debian:stable-slim
+
+docker container ls -l
+
+docker container start aade3d82ae41
+
+docker container ls -a
+
+docker container attach aade
+
+docker container rm aade
