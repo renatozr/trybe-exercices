5f68bf2bd741c29be57b755fed529fd1d4c6ad3f
Renato Zanella <renatozr07@gmail.com>
2022-09-19 15:47:31 -0300

    add: exercício 3

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/Baralho.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/Baralho.py
@@ -26,23 +26,47 @@ class IteradorDoBaralho(Iterator):
         return carta
 
 
+class IteradorDoBaralhoInverso(Iterator):
+    def __init__(self, cartas):
+        self.cartas = cartas
+        self.indice = len(cartas) - 1
+
+    def __next__(self):
+        try:
+            carta = self.cartas[self.indice]
+        except IndexError:
+            raise StopIteration
+
+        self.indice -= 1
+
+        return carta
+
+
 class Baralho(Iterable):
     naipes = "copas ouros espadas paus".split()
     valores = "A 2 3 4 5 6 7 8 9 10 J Q K".split()
 
     def __init__(self):
-        self.__cartas = [
+        self._cartas = [
             Carta(valor, naipe)
             for naipe in self.naipes
             for valor in self.valores
         ]
 
     def __len__(self):
-        return len(self.__cartas)
+        return len(self._cartas)
 
     def __iter__(self):
-        return IteradorDoBaralho(self.__cartas)
+        return IteradorDoBaralho(self._cartas)
 
 
-for carta in Baralho():
+class BaralhoInverso(Baralho):
+    def __init__(self):
+        super().__init__()
+
+    def __iter__(self):
+        return IteradorDoBaralhoInverso(self._cartas)
+
+
+for carta in BaralhoInverso():
     print(carta)
